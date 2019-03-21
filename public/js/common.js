5de02e6e746745bc2577928343cba0d4e81fd9a0
tbj <tangbaojie@126.com>
2019-03-21 10:13:27 +0800

    提交seed

---
+++ public/js/common.js
@@ -0,0 +1,11 @@
+
+document.write("<link rel='stylesheet' href='../h5/css/ydui.css?time="+Math.random()+"'>");
+document.write("<link rel='stylesheet' href='../h5/css/bb.css?time="+Math.random()+"'>");
+
+document.write('<script src="js/jquery.min.js"  type="text/javascript"></script>');
+document.write('<script src="js/vue.min.js"  type="text/javascript"></script>');
+document.write("<script type='text/javascript' src='js/vue-resource.js?time="+Math.random()+"'><\/script>");
+document.write('<script src="js/layer/layer.js"  type="text/javascript"></script>');
+document.write('<script src="js/ydui.js"  type="text/javascript"></script>');
+document.write('<script src="js/ydui.flexible.js"  type="text/javascript"></script>');
+document.write('<script src="js/clipboard.min.js"  type="text/javascript"></script>');
